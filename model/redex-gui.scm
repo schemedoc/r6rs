5a43bf2607cfcc4945d99c867a685a85f56fb798
findler <unknown>
2007-05-10 16:20:35 +0000

    removed obsolete files

--- model/redex-gui.scm
+++
@@ -1,3 +0,0 @@
-(module redex-gui mzscheme
-  (require (planet "gui.ss" ("robby" "redex.plt" 3)))
-  (provide (all-from (planet "gui.ss" ("robby" "redex.plt" 3)))))
--- model/redex.scm
+++
@@ -1,6 +0,0 @@
-(module redex mzscheme
-  (require (planet "reduction-semantics.ss" ("robby" "redex.plt" 3))
-           (planet "subst.ss" ("robby" "redex.plt" 3)))
-  (provide (all-from (planet "reduction-semantics.ss" ("robby" "redex.plt" 3)))
-           (all-from (planet "subst.ss" ("robby" "redex.plt" 3)))))
-
